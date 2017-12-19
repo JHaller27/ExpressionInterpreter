80bc67a63e56edb5711038c6cf3faaea8787974b
James <jameshaller27@gmail.com>
2017-12-19 00:06:04 -0500

    Update for python3 & fix indents

--- src/ExpressionInterpreter.py
+++ src/ExpressionInterpreter.py
@@ -3,41 +3,33 @@ OPERATORS = '+-*/^%'
 
 
 class Node:
-  __slots__ = 'left, right, data'
+    __slots__ = ['data', 'left', 'right']
 
-  def Node(self):
-    left = None
-    right = None
-    data = 0
-
-  def Node(self, left, data, right):
-    self.left = left
-    self.data = data
-    self.right = right
-
-  def Node(self, data):
-    self(None, data, None)
+    def __init__(self, data=None, left=None, right=0):
+        self.data = data
+        self.left = left
+        self.right = right
 
 
 def tokenize(base):
-  tokens = []
-  base.replace(' ', '')
-  idx = 0
-  while idx < len(base):
-    ch = base[idx]
-    if ch in DIGITS or ch in OPERATORS:
-      tokens.append(ch)
-    elif ch is '(':
-      token = []
-      idx += 1
-      ch = base[idx]
-      while ch is not ')':
-        token.append(ch)
-        idx += 1
+    tokens = []
+    base.replace(' ', '')
+    idx = 0
+    while idx < len(base):
         ch = base[idx]
-      tokens.append(token)
-    idx += 1
-  return tokens
+        if ch in DIGITS or ch in OPERATORS:
+            tokens.append(ch)
+        elif ch is '(':
+            token = []
+            idx += 1
+            ch = base[idx]
+            while ch is not ')':
+                token.append(ch)
+                idx += 1
+                ch = base[idx]
+            tokens.append(token)
+        idx += 1
+    return tokens
 
 
 def main():
@@ -46,4 +38,4 @@ def main():
 
 
 if __name__ == '__main__':
-  main()
+    main()
